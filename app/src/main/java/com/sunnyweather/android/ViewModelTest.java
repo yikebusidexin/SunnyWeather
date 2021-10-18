874c0ec71298e07301966e024dd49ec4cca76348
huanghongwei <2020489274@qq.com>
2021-10-18 16:45:22 +0800

    First commit .

---
+++ app/src/main/java/com/sunnyweather/android/ViewModelTest.java
@@ -0,0 +1,52 @@
+package com.sunnyweather.android;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.AndroidViewModel;
+
+public class ViewModelTest extends AndroidViewModel {
+    public ViewModelTest(@NonNull Application application) {
+        super(application);
+    }
+
+    @NonNull
+    @Override
+    public <T extends Application> T getApplication() {
+        return super.getApplication();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
+
+    @NonNull
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+    
+}
